f847b6cf2b0c6248bcd2c870827ca40eee0e464e
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2024-11-25 20:00:32 -0500

    Initial

---
+++ LeekPlugin.R
@@ -0,0 +1,44 @@
+###################################################
+### code chunk number 1: sva.Rnw:5-6
+###################################################
+options(width=65)
+
+
+###################################################
+### code chunk number 3: input
+###################################################
+library(sva)
+library(bladderbatch)
+#data(bladderdata)
+library(pamr)
+library(limma)
+
+
+dyn.load(paste("RPluMA", .Platform$dynlib.ext, sep=""))
+source("RPluMA.R")
+
+input <- function(inputfile) {
+  parameters <<- read.table(inputfile, as.is=T);
+  rownames(parameters) <<- parameters[,1];
+    pfix = prefix()
+  if (length(pfix) != 0) {
+     pfix <<- paste(pfix, "/", sep="")
+  }
+}
+
+run <- function() {}
+
+output <- function(outputfile) {
+
+###################################################
+### code chunk number 4: input
+###################################################
+#pheno = pData(bladderEset)
+pheno = read.csv(paste(pfix, parameters["pheno", 2], sep="/"))
+edata = as.matrix(read.csv(paste(pfix, parameters["edata", 2], sep="/")))
+mod = model.matrix(~as.factor(cancer), data=pheno)
+mod0 = model.matrix(~1,data=pheno)
+n.sv = num.sv(edata,mod,method="leek")
+print(n.sv)
+write.csv(n.sv, outputfile)
+}
